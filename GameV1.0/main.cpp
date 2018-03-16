1cbac5c4ad615caa8e46b861585c67b2f9c4a613
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2018-03-16 23:28:37 +0300

    Initial Commit

---
+++ GameV1.0/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameV1.0
+//
+//  Created by ILdar Zinatulin on 16.03.18.
+//  Copyright © 2018 ILdar Zinatulin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
