283903083bcbb901b68cd11e0b837fa50fe7e37d
ILdar Zinatulin <ildarzinatulin@MacBook-Air-ILdar.local>
2018-03-17 12:56:07 +0300

    first commit

---
+++ GameV1.0/Units.cpp
@@ -0,0 +1,9 @@
+//
+//  Units.cpp
+//  GameV1.0
+//
+//  Created by ILdar Zinatulin on 16.03.18.
+//  Copyright © 2018 ILdar Zinatulin. All rights reserved.
+//
+
+#include "Units.hpp"
---
+++ GameV1.0/Units.hpp
@@ -0,0 +1,14 @@
+//
+//  Units.hpp
+//  GameV1.0
+//
+//  Created by ILdar Zinatulin on 16.03.18.
+//  Copyright © 2018 ILdar Zinatulin. All rights reserved.
+//
+
+#ifndef Units_hpp
+#define Units_hpp
+
+#include <stdio.h>
+
+#endif /* Units_hpp */
