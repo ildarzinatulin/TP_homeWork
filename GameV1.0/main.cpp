190782c9beff02c20a548bac20356413b926f6cb
ildarzinatulin <36409769+ildarzinatulin@users.noreply.github.com>
2018-03-17 13:19:26 +0300

    Update main.cpp

--- GameV1.0/main.cpp
+++ GameV1.0/main.cpp
@@ -1,15 +1,13 @@
-//
-//  main.cpp
-//  GameV1.0
-//
-//  Created by ILdar Zinatulin on 16.03.18.
-//  Copyright © 2018 ILdar Zinatulin. All rights reserved.
-//
-
 #include <iostream>
+#include "Units.cpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    ArmyCreater creater;
+    PeopleArmyFactory peopleFactory;
+    OrcsArmyFactory orcsFactory;
+    ElfsArmyFactory elfsFactory;
+    
+   Army *people = creater.create(peopleFactory);
+    
     return 0;
 }
